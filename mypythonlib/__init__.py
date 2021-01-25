cea7b49e000929330e5ff97d667758142fcadec8
Andrey Golik <1858191+andreygolik@users.noreply.github.com>
2021-01-25 16:39:44 -0500

    init

---
+++ mypythonlib/__init__.py
@@ -0,0 +1,4 @@
+# Basically, any folder that has an __init__.py file in it, will be included in the library when we build it.
+# Most of the time, you can leave the __init__.py files empty.
+# Upon import, the code within __init__.py gets executed, so it should contain only the minimal amount of code
+# that is needed to be able to run your project.
---
+++ mypythonlib/myfunctions.py
@@ -0,0 +1,13 @@
+def square(x: float) -> float:
+    """Calculate the square of X
+
+    Args:
+        x (float): value
+
+    Returns:
+        float: square of x (x^2)
+    """
+
+    square = x**2
+
+    return square
